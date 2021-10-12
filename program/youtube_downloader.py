62a18c2a97f3bb1b3fbf8ee48c643220fc02e188
George Ceja <Net.george.sky@gmail.com>
2021-10-12 01:12:43 -0700

    add json library to generate file to track music being downloaded

--- program/youtube_downloader.py
+++ program/youtube_downloader.py
@@ -1,7 +1,10 @@
 from pytube import YouTube
 from docstrings import *
+import dump from json
 
-downloads_folder = "../my_music"
+data = {}
+data['videos'] = []
+music_file_path = "../my_music"
 
 def select(option):
     if option in "":
@@ -24,6 +27,10 @@ def select(option):
     else:
         print(f"{RED}[*]Sorry that doesn't look like a command{ENDC}")
 
+def append_video_to_json(file_path, file_name):
+    # TODO: Handle dumping music data to data object
+    print(f"{YELLOW}Added video details to video.txt{ENDC}")
+
 def get_url_and_name():
     print("\nYoutube Music Downloader")
     print(medium_line)
@@ -43,7 +50,10 @@ def download_video(video_url, file_name):
         if file_name:
             youtube_video.title = file_name
 
-        youtube_video.streams.first().download(output_path=downloads_folder)
+        youtube_video.streams.first().download(output_path=music_file_path)
+
+        # For now append to music.txt for each downloaded video; handle dups with JS
+        append_video_to_json(music_file_path+file_name, file_name)
     except:
         print(f"{RED}[*] Video was not able to be downloaded{ENDC}")
 
